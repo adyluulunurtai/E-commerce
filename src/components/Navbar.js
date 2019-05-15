8b0e5aa1131b8292aa61a2b08b143b5108f80c8d
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-15 16:30:33 +0600

    stylizing navbar bg and cart button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import styled from "styled-components";
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <nav className="navbar navbar-expand-sm bg-warning navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -18,14 +18,35 @@ class Navbar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             korzina
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background-color: transparent;
+  border: 0.05rem solid var(--mainYellow);
+  color: var(--mainDark);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: var(--mainYellow);
+    color: var(--mainWhite);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default Navbar;
