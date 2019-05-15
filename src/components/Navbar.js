389361c392b1c72d14572ad24c9fd93ecbb592c5
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-15 17:32:19 +0600

    ended make navbar.js and stylizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
 import { ButtonContainer } from "./Button";
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-warning navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -25,9 +26,17 @@ class Navbar extends Component {
             korzina
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+const NavWrapper = styled.nav`
+  background: rgb(235, 183, 72);
+  .nav-link {
+    color: var(--mainDark) !important;
+    font-size: 1.4rem;
+    text-transform: capitalize;
+  }
+`;
 
 export default Navbar;
