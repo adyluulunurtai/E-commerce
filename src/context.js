934c07f786609050616c9f62f28edc9140325d53
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-17 15:33:53 +0600

    change simple value at context.js  to data from data.js

--- src/context.js
+++ src/context.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
