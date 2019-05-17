2d2836a49a65b20b3a7f287c77b04ee00e38166a
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-17 15:01:37 +0600

    wrap App <ProductProvider>

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { ProoductProvider } from "./context";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ProoductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProoductProvider>,
   document.getElementById("root")
 );
 
