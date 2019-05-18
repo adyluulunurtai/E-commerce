9bd48ba36912cac0a24fc6325948f4bec439f65e
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-18 16:52:25 +0600

    begining product.js and  make basic stylizing

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 class Product extends Component {
   render() {
     return (
-      <div className="Product">
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
         <h3>HEllo from Product</h3>
-      </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
 
 export default Product;
