0e84441a4ba4b11b215ff8168fb9b1f359cffd29
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-18 17:33:03 +0600

    added initial button "add to cart" in product

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import { ProductConsumer } from "../context";
 
 class Product extends Component {
   render() {
-    const { id, title, img, price, incart } = this.props.product;
+    const { id, title, img, price, inCart } = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
         <div className="card">
@@ -13,6 +13,22 @@ class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top" />
             </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to the cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  {" "}
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
