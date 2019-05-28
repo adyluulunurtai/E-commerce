bb1cca6b0d89a66fac098716cad4f919c114a03b
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-28 15:24:41 +0600

    add img and info in details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,50 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
 
 class Details extends Component {
   render() {
     return (
-      <div className="Details">
-        <h3>hello from Details</h3>
-      </div>
+      <ProductConsumer>
+        {value => {
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-5 ">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-5 text-slanted ">
+                  <h2>модель : {title}</h2>
+                  <h4 className=" mt-3 mb-2 text-capitalize">
+                    компания : {company}
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>Цена : {price} сом</strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    описание :{" "}
+                  </p>
+                  <p className="text-muted lead">{info}</p>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
     );
   }
 }
